856d0eed90557565a986bba916360dce2a039c04
wadood <wadoodislam@gmail.com>
2021-07-15 03:51:47 +0500

    fixed rules for northeastlantern_spider.py

--- industrysync/spiders/northeastlantern_spider.py
+++ industrysync/spiders/northeastlantern_spider.py
@@ -6,30 +6,27 @@ from scrapy.linkextractors import LinkExtractor
 class NortheastlanternSpider(CrawlSpider):
     name = 'northeastlantern_crawl'
     allowed_domains = ['www.northeastlantern.com']
-    start_urls = ['https://www.northeastlantern.com']
+    start_urls = ['https://www.northeastlantern.com/view/category/']
     rules = (
-        Rule(LinkExtractor(restrict_css='.navcolumn-3 a')),
-        Rule(LinkExtractor(restrict_css='.viewlist li a')),
-        Rule(LinkExtractor(restrict_css='a.hover-view'), callback='parse_item')
+        Rule(LinkExtractor(restrict_css='[title="View products in this category"]')),
+        Rule(LinkExtractor(restrict_css='[title="View product"]'), callback='parse_item')
     )
     custom_settings = {
         'CRAWLERA_ENABLED': False
     }
 
     def parse_item(self, response):
-        identity = response.css('.productcodetitle')
-        if not identity:
-            return
         spec_sheet = response.css('.spec-sheet::attr(href)').get()
+        css = '.productdetails :contains("{}")::text'
         item = {
             'product-code': f'{response.css(".ProductCode::Text").get()[:-1]}',
             'title': response.css('.productseperator h2::text').get(),
             'images': ';'.join([response.urljoin(url) for url in response.css('.sidethumbs li img::attr(src)').getall()]),
-            'details-dimensions': response.css('.productdetails :contains("Dimensions")::text').get().replace("Dimensions: ", ''),
-            'details-mounting-height': response.css('.productdetails :contains("Mounting height")::text').get().replace('Mounting height: ', ''),
-            'details-bulb-type': response.css('.productdetails :contains("Bulb")::text').get().replace('Bulb type: ', ''),
-            'details-suitable-location': response.css('.productdetails :contains("Suitable")::text').get().replace('Suitable location: ', ''),
-            'details-mounting-area': response.css('.productdetails :contains("Mounting Area")::text').get().replace('Mounting Area: ', ''),
+            'details-dimensions': (response.css(css.format("Dimensions")).get() or '').replace("Dimensions: ", ''),
+            'details-mounting-height': (response.css(css.format("Mounting height")).get() or '').replace('Mounting height: ', ''),
+            'details-bulb-type': (response.css(css.format("Bulb")).get() or '').replace('Bulb type: ', ''),
+            'details-suitable-location': (response.css(css.format("Suitable")).get() or '').replace('Suitable location: ', ''),
+            'details-mounting-area': (response.css(css.format("Mounting Area")).get() or '').replace('Mounting Area: ', ''),
             'Other-sizes-available': ';'.join([size.strip() for size in response.css('.relatedsizes li::text').getall()]),
             'finish-options': ';'.join([response.urljoin(url) for url in response.css('.finishoptions ::attr(src)').getall()]),
             'glass-options': ';'.join([response.urljoin(url) for url in response.css('.glassoptions ::attr(src)').getall()]),
