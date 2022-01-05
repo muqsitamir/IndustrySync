21b2b08eee22ca95a57543d7834c5931f28d63a8
wadood <wadoodislam@gmail.com>
2022-01-06 04:27:43 +0500

    added schonbek1870 spider

---
+++ industrysync/spiders/schonbek1870_spider.py
@@ -0,0 +1,35 @@
+from scrapy import FormRequest
+from scrapy.spiders import CrawlSpider
+
+
+class Schonbek1870Spider(CrawlSpider):
+    name = 'schonbek1870_crawl'
+    allowed_domains = ['ws.schonbek.com']
+    start_urls = ['https://ws.schonbek.com/ws/ext/login.html']
+    inventory_url = 'https://ws.schonbek.com/ws/ext/z_repavailfixinventory.html'
+    custom_settings = {
+        'CRAWLERA_ENABLED': False
+    }
+
+    def start_requests(self):
+        payload = {
+            'Name': 'jkrupp',
+            'password': '@Chloe071135@',
+            'Submit': 'login',
+            'action': 'login'
+        }
+        return [FormRequest(url=self.start_urls[0], formdata=payload, callback=self.parse_start_url)]
+
+    def parse_start_url(self, response, **kwargs):
+        body = 'electrics=100&electrics=110&electrics=120-240&electrics=12V&electrics=220&electrics=24V' \
+               '&electrics=CANDLE&electrics=LOW%2BVOLT&electrics=NONE&action=submit'
+        return FormRequest(url=self.inventory_url, method='POST', headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                           body=body, callback=self.parse_items)
+
+    def parse_items(self, response):
+        keys = response.css('thead th::text').extract()
+        for row_s in response.css('.tableTbody tr'):
+            values = row_s.css('img::attr(src),td::text').extract()
+            product = {k: v for k, v in zip(keys, values)}
+            product['Image'] = response.urljoin(product['Image'])
+            yield product
