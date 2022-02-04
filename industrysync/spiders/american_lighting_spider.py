2845f18002408f88caf7c3ddac38d07761274236
wadood <wadoodislam@gmail.com>
2022-02-04 05:16:47 +0500

    added schonbekdisco_spider.py and american_lighting_spider.py

---
+++ industrysync/spiders/american_lighting_spider.py
@@ -0,0 +1,56 @@
+from scrapy.link import Link
+from scrapy.spiders import CrawlSpider
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
+from w3lib.url import add_or_replace_parameter
+
+
+class PaginationLE:
+    page_size = 36
+
+    def extract_links(self, response):
+        no_of_products = response.css('#record-count::attr(value)').get()
+
+        if not no_of_products:
+            return []
+
+        return [Link(add_or_replace_parameter(response.url, 'page', int(page)))
+                for page in range(2, int(no_of_products)//self.page_size + 2)]
+
+
+class AmericanLightingSpider(CrawlSpider):
+    name = 'american_lighting_crawl'
+    allowed_domains = ["framburg.com", "houseoftroy.com", "thumprints.com", "arroyocraftsman.com"]
+    start_urls = [
+        "https://framburg.com/categories",
+        "https://houseoftroy.com/categories",
+        "https://thumprints.com/categories",
+        "https://arroyocraftsman.com/categories"
+    ]
+
+    custom_settings = {
+        'CRAWLERA_ENABLED': False
+    }
+
+    rules = (
+        Rule(PaginationLE()),
+        Rule(LinkExtractor(restrict_css=".p-container + #container .item")),
+        Rule(LinkExtractor(restrict_css="#products .item"), callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        product_name = response.css("#product-details>h1.item-name::text").get()
+        item = {
+            "title": product_name,
+            "sku": product_name.split()[-1],
+            "download_url": response.urljoin(response.css("#tearsheetVueApp>a")[1].css("::attr(href)").get()),
+            "text": response.css("#details .item-description::text").get(),
+            "image_urls": ';'.join([image_url.split('?')[0] for image_url in response.css("#alt-slideshow img::attr(src)").getall()]),
+            "features": {},
+        }
+
+        for idx, feature in enumerate(response.css("#details li")):
+            key, value = feature.css("::text").getall()
+            item['features'][str(idx)] = {key[:-1]: value.strip()}
+
+        return item
---
+++ industrysync/spiders/schonbekdisco_spider.py
@@ -0,0 +1,56 @@
+from urllib.parse import quote_plus
+
+import scrapy
+from scrapy.spiders import CrawlSpider
+from scrapy import FormRequest, Request
+from datetime import datetime, timedelta
+
+
+class SchonbekDiscoSpider(CrawlSpider):
+    name = 'schonbek_disco_crawl'
+    allowed_domains = ['schonbek.com']
+    start_urls = ["https://ws.schonbek.com/ws/ext/login.html"]
+    
+    custom_settings = {
+        'CRAWLERA_ENABLED': False
+    }
+
+    def start_requests(self):
+        form_data = {
+            "Name": "jkrupp",
+            "password": "@Chloe071135@",
+            "Submit": "login",
+            "action": "login",
+        }
+        return [FormRequest(url=self.start_urls[0], formdata=form_data, callback=self.parse_login)]
+
+    def parse_login(self, response):
+        return scrapy.Request("https://ws.schonbek.com/ws/ext/z_retired-fixtures-list.html",
+                              self.parse_fixtures_form)
+
+    def parse_fixtures_form(self, response):
+        url = "https://ws.schonbek.com/ws/ext/z_retired-fixtures-list.html"
+        date_to = datetime.today()
+        date_from = date_to.replace(year=date_to.year-10)
+        payload = 'electrics=100&electrics=110&electrics=120-240&electrics=127V&electrics' \
+                  '=12V&electrics=220&electrics=24V&electrics=CANDLE&electrics=LOW+VOLT&' \
+                  f'electrics=NONE&datefrom={quote_plus(date_from.strftime("%d/%m/%y"))}&d' \
+                  f'ateto={quote_plus(date_to.strftime("%d/%m/%y"))}&action=submit'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        return Request(url=url, method='POST', body=payload,
+                       headers=headers, callback=self.parse_data)
+
+    def parse_data(self, response):
+        for row in response.css(".tableTbody tr"):
+            data = row.css("td")
+            item = {
+                "product_line": data[0].css('::text').get(),
+                "core": data[1].css('::text').get(),
+                "fixture": data[2].css('::text').get(),
+                "description": data[3].css('::text').get(),
+                "voltage/electrics": data[4].css('::text').get(),
+                "discontinued date": data[5].css('::text').get(),
+            }
+            yield item
